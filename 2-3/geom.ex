f38d668a05ae51b71e8323449dcc56f6571ef018
Lucian Feroiu <lucianferoiu@gmail.com>
2015-09-05 10:39:00 +0300

    Etude 2-3

---
+++ 2-3/geom.ex
@@ -0,0 +1,17 @@
+defmodule Geom do
+	@moduledoc """
+		Trivial geometry module, part of the "Études for Elixir" exercises
+	"""
+
+	@doc """
+	Calculates the area of a rectangle, provided width and height as parameters.
+	The height is implicitly equal to 1, if the argument is not provided
+
+		iex> Geom.area 5, 6
+		30
+
+	"""
+	def area(w, h \\ 1.0) when is_number(w) do
+			w*h
+	end
+end
