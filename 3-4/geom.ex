156328998d8c17db82e32a40adb882998439776e
Lucian Feroiu <lucianferoiu@gmail.com>
2015-09-05 12:30:27 +0300

    Étude 3-4

--- 3-4/geom.ex
+++ 3-4/geom.ex
@@ -1,8 +1,8 @@
 defmodule Geom do
-	def area(shape,p1,p2) when p1>=0 and p2>=0 do
+	def area({shape,p1,p2}) when p1>=0 and p2>=0 do
 		_area(shape,p1,p2)
 	end
-	def area(_,_,_), do: 0
+	def area({_,_,_}), do: 0
 
 	defp _area(:rectangle, w, h), do:	w*h
 	defp _area(:triangle,b,h), do: b*h/2.0
