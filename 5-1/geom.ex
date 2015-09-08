231fa4cd1f6d2d0060d2c2d7efaf0608f5d9123e
Lucian Feroiu <lucianferoiu@gmail.com>
2015-09-08 10:19:09 +0300

    Étude 5-1

--- 5-1/geom.ex
+++ 5-1/geom.ex
@@ -1,8 +1,8 @@
 defmodule Geom do
-	def area({shape,p1,p2}) when p1>=0 and p2>=0 do
+	def area(shape,p1,p2) when p1>=0 and p2>=0 do
 		_area(shape,p1,p2)
 	end
-	def area({_,_,_}), do: 0
+	def area(_,_,_), do: 0
 
 	def main do
 		prompt_user
@@ -12,20 +12,20 @@ defmodule Geom do
 		shape = prompt_shape |> shape_to_atom
 		IO.puts "Shape is #{shape}"
 		if shape != :quit do
-			p1 = prompt_number(1) |> String.to_integer
-			p2 = prompt_number(2) |> String.to_integer
-			val = area({shape,p1,p2})
+			p1 = prompt_number(1) |> String.to_float
+			p2 = prompt_number(2) |> String.to_float
+			val = area(shape,p1,p2)
 			IO.puts "Result: #{val}"
 			prompt_user
 		end
 	end
 
 	def prompt_shape do
-		IO.gets "Shape: R)ectangle / E)llipse / T)riangle / Q)uit: " |> String.strip |> String.upcase
+		ch = IO.gets "Shape: R)ectangle / E)llipse / T)riangle / Q)uit: "
+		String.strip(ch,?\n) |> String.upcase
 	end
 
 	def shape_to_atom(ch) do
-		IO.puts "Read #{ch}"
 		case ch do
 			"R" -> :rectangle
 			"E" -> :ellipsis
@@ -35,7 +35,8 @@ defmodule Geom do
 	end
 
 	def prompt_number(ord) do
-		IO.gets "Parameter #{ord}: " |> String.strip
+		nr = IO.gets "Parameter #{ord}: "
+		String.strip nr,?\n
 	end
 
 	defp _area(:rectangle, w, h), do:	w*h
