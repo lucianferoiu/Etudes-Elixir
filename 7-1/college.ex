14687418059dfbe7f6f98807e3d13e579417d6a5
Lucian Feroiu <lucianferoiu@gmail.com>
2015-09-12 13:19:29 +0300

    Étude 7-1

--- 7-1/college.ex
+++ 7-1/college.ex
@@ -7,15 +7,19 @@ defmodule College do
 
 	defp parse_rooms(hd,fd) do
 		case IO.read(fd,:line) do
-			data -> add_course(hd,data,fd)
-			{:eof} -> hd
+			:eof -> hd
 			{:error, reason} ->
 				IO.puts("Error reading file: #{reason}")
 				hd
+			data -> add_course(hd,data,fd)
 		end
 	end
 
-	defp add_course(hd,data,fd) do
-		
+	defp add_course(hd,line,fd) do
+		[cid,ctitle,room] = String.strip(line) |> String.split(",")
+		existing_courses = Dict.get(hd,room,[])
+		courses = [{String.to_integer(cid),ctitle}|existing_courses]
+		hd2 = Dict.put(hd,room,courses)
+		parse_rooms(hd2,fd)
 	end
 end
