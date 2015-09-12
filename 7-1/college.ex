20b16b2ab8324cc4548f253b2df0c4c3b67114d0
Lucian Feroiu <lucianferoiu@gmail.com>
2015-09-12 12:42:02 +0300

    Étude 7-1 - WIP

--- 7-1/college.ex
+++ 7-1/college.ex
@@ -1,5 +1,14 @@
 defmodule College do
 	def rooms do
 		{:ok, fd} = File.open("courses.csv",[:read, :utf8])
+		hd = HashDict.new
+		parse_rooms(hd,fd)
+	end
+
+	defp parse_rooms(hd,fd) do
+		case IO.readline(fd) do
+			
+			{:eof} -> hd
+		end
 	end
 end
