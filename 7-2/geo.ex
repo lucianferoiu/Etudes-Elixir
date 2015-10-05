6b07715e063cfa97e3f86b81b63f49923802b4ae
Lucian Feroiu <lucianferoiu@gmail.com>
2015-10-05 11:37:56 +0300

    Etude 7-2

--- 7-2/geo.ex
+++ 7-2/geo.ex
@@ -6,19 +6,47 @@ defmodule City do
 	defstruct name: nil, lat: 0.0, long: 0.0, population: 0
 end
 
+defimpl Inspect, for: City do
+	import Inspect.Algebra
+	def inspect(data,_opts) do
+		[data.name, " (", to_string(data.lat), ",", to_string(data.long),") - ",to_string(data.population)," inhabitants", break] |> concat
+	end
+end
+
+defimpl Inspect, for: Country do
+	import Inspect.Algebra
+	def inspect(data, opts) do
+		[break("\n"), data.name, break, " - official language: ", data.lang, ", cities: ", break("\n"), data.cities |> to_doc(opts) , break("\n")] |> concat
+	end
+end
+
 defmodule Geo do
 	def make_geo_list(filename) do
 		{:ok, fd} = File.open(filename,[:read, :utf8])
-		countries = parse_file(fd,[])
+		parse_file(fd,[]) |> Enum.reverse |> IO.inspect
 	end
 
 	def parse_file(fd,countries) do
-		case File.read(fd,:line) of
+		case IO.read(fd,:line) do
 			:eof -> countries
-			{:error,reason} -> IO.puts "Erorr reading file: #{reason}"
+			{:error,reason} ->
+				IO.puts "Erorr reading file: #{reason}"
 			line ->
 				parse_file(fd,update_countries(countries,line))
 		end
+	end
+
+	def update_countries(countries, line) do
+		entries = String.strip(line) |> String.split(",")
+		case entries do
+			[country,lang] ->
+				[%Country{name: country, lang: lang}|countries]
+			[city, pop, lat, long] ->
+				new_city = %City{name: city, lat: String.to_float(lat), long: String.to_float(long), population: String.to_integer(pop)}
+				[latest_country| other_countries] = countries
+				[%Country{latest_country | cities: [new_city|latest_country.cities]} | other_countries]
+		end
+
 	end
 
 end
