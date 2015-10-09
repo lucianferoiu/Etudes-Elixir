8a8d6ad6ced1f689c57305fcb2e1afaf673eaccf
Lucian Feroiu <lucianferoiu@gmail.com>
2015-10-09 09:59:11 +0300

    Etude 8-5 - addendum

--- 8-5/cards.ex
+++ 8-5/cards.ex
@@ -3,6 +3,7 @@ defmodule Cards do
 		colors = ["♥","♦","♣","♠"]
 		numbers = for i <- 2..10, do: Integer.to_string(i)
 		ranks = ["A"| numbers] ++ ["J", "Q", "K"]
+		# lc .. inlist was deprecated in Elixir v0.13.2
 		for c <- colors, r <- ranks, do: {r,c}
 	end
 end
